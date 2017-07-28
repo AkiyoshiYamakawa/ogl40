2281b6c6d46b543f204a64599a461cc41c5210a5
AkiyoshiYamakawa <yamakawa-akiyoshi@kbc.ac.jp>
2017-07-28 17:43:48 +0900

    vec3 対応, a / s / d / w / q / e キーによる球体の移動。

---
+++ ingredients/scene.h
@@ -0,0 +1,40 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <glm/glm.hpp>
+using glm::vec3;
+
+class Scene
+{
+public:
+	Scene() : m_animate(true) { }
+	
+    /**
+      Load textures, initialize shaders, etc.
+      */
+    virtual void initScene() = 0;
+
+    /**
+      This is called prior to every frame.  Use this
+      to update your animation.
+      */
+    virtual void update( float t, vec3 position ) = 0;
+
+    /**
+      Draw your scene.
+      */
+    virtual void render() = 0;
+
+    /**
+      Called when screen is resized
+      */
+    virtual void resize(int, int) = 0;
+    
+    void animate( bool value ) { m_animate = value; }
+    bool animating() { return m_animate; }
+    
+protected:
+	bool m_animate;
+};
+
+#endif // SCENE_H
